942c134998ed2e61a2aaff63cdc74a250e640ad7
Eyal Lotem <eyal.lotem@gmail.com>
2010-06-10 10:34:17 +0300

    Separate main helpers to their own top-level functions

--- Editor.hs
+++ Editor.hs
@@ -12,7 +12,7 @@ import Graphics.UI.VtyWidgets.Widget(Widget)
 import qualified Graphics.UI.VtyWidgets.TextEdit as TextEdit
 import qualified Graphics.UI.VtyWidgets.Grid as Grid
 import Graphics.UI.VtyWidgets.Run(runWidgetLoop)
-import Data.IORef(newIORef, readIORef, writeIORef)
+import Data.IORef(IORef, newIORef, readIORef, writeIORef)
 
 main :: IO ()
 main = do
@@ -22,23 +22,24 @@ main = do
   makeTextEdits <- mapM (makeTextEditMaker db) irefs
   gridModelR <- newIORef Grid.initModel
   runWidgetLoop . const $ makeWidget gridModelR makeTextEdits
+
+makeWidget :: IORef Grid.Model -> [IO (Widget (IO ()))] -> IO (Widget (IO ()))
+makeWidget gridModelR makeTextEdits = do
+  textEdits <- sequence makeTextEdits
+  let items = map (return . (,) True) textEdits
+  Grid.make (writeIORef gridModelR) items `fmap`
+    readIORef gridModelR
+
+makeTextEditMaker :: ObjectStore -> IRef String -> IO (IO (Widget (IO ())))
+makeTextEditMaker db iref = do
+  let deref = ObjectStore.lookup db iref
+  text <- deref
+  cursorR <- newIORef (length text)
+  return $ do
+    textEditModel <- liftM2 TextEdit.Model (readIORef cursorR) deref
+    return . fmap (handleTextEditModel cursorR) $
+      TextEdit.make 1 TextEdit.defaultAttr TextEdit.editingAttr textEditModel
   where
-    makeWidget gridModelR makeTextEdits = do
-        textEdits <- sequence makeTextEdits
-        let items = map (return . (,) True) textEdits
-        Grid.make (writeIORef gridModelR) items `fmap`
-          readIORef gridModelR
-
-    makeTextEditMaker :: ObjectStore -> IRef String -> IO (IO (Widget (IO ())))
-    makeTextEditMaker db iref = do
-      let deref = ObjectStore.lookup db iref
-      text <- deref
-      cursorR <- newIORef (length text)
-      return $ do
-        textEditModel <- liftM2 TextEdit.Model (readIORef cursorR) deref
-        return . fmap (handleTextEditModel db iref cursorR) $
-          TextEdit.make 1 TextEdit.defaultAttr TextEdit.editingAttr textEditModel
-
-    handleTextEditModel db iref cursorR (TextEdit.Model cursor text) = do
+    handleTextEditModel cursorR (TextEdit.Model cursor text) = do
       writeIORef cursorR cursor
       ObjectStore.set db iref text
