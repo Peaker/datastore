b3748d92336fa5b4699210f57e51bc08059fd75b
Eyal Lotem <eyal.lotem@gmail.com>
2010-06-22 21:38:26 +0300

    Add toxml

---
+++ src/toxml.hs
@@ -0,0 +1,30 @@
+{-# OPTIONS -O2 -Wall #-}
+
+module Main (main) where
+
+import qualified Db
+import Db(Db)
+import qualified Tree
+import qualified Ref
+import qualified Graphics.UI.VtyWidgets.TextEdit as TextEdit
+import System.IO(stdout, hPutStrLn, Handle)
+import Data.ByteString.UTF8(fromString)
+import qualified Data.Record.Label as Label
+
+writeTreeXml :: Db -> Handle -> Int -> Tree.Ref -> IO ()
+writeTreeXml db outFile depth ref = do
+  tree <- Ref.read db ref
+  value <- Ref.read db (Label.get Tree.nodeValueRef tree)
+  let text = TextEdit.textEditText . snd $ value
+      indent = (replicate (2 * depth) ' ' ++)
+  hPutStrLn outFile . indent $ "<" ++ text ++ ">"
+  mapM_ (writeTreeXml db outFile (depth + 1)) $
+    Label.get Tree.nodeChildrenRefs tree
+  hPutStrLn outFile . indent $ "</" ++ text ++ ">"
+
+main :: IO ()
+main = do
+  Db.withDb "/tmp/db.db" $ \db -> do
+    Just root <- Db.lookup db (fromString "root")
+                 -- root :: Tree.Ref
+    writeTreeXml db stdout 0 root
