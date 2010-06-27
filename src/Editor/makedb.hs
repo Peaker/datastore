8725c07d8053600a25e1ef8bb3e16c4b0ffcd91c
Eyal Lotem <eyal.lotem@gmail.com>
2010-06-27 22:39:39 +0300

    Qualified imports

--- src/Editor/makedb.hs
+++ src/Editor/makedb.hs
@@ -2,15 +2,15 @@
 
 module Main (main) where
 
-import qualified Db
 import Data.ByteString.UTF8(fromString)
-import Editor.Tree(Ref, makeLeafRef, makeNodeRef)
+import qualified Db
+import qualified Editor.Tree as Tree
 
 main :: IO ()
 main =
   Db.withDb "/tmp/db.db" $ \db -> do
-    childrenRefs <- mapM (makeLeafRef db . show) [1..10 :: Int]
-    rootRef <- makeNodeRef db "tree root value" childrenRefs
+    childrenRefs <- mapM (Tree.makeLeafRef db . show) [1..10 :: Int]
+    rootRef <- Tree.makeNodeRef db "tree root value" childrenRefs
     Db.set db (fromString "root") rootRef
     Db.set db (fromString "viewroot") rootRef
-    Db.set db (fromString "clipboard") ([] :: [Ref])
+    Db.set db (fromString "clipboard") ([] :: [Tree.Ref])
