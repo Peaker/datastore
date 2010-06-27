64c20d25649e4a9b23ddf302ec4701dab48c66d1
Eyal Lotem <eyal.lotem@gmail.com>
2010-06-27 23:38:01 +0300

    Add BS and Guid xor

--- src/Data/ByteString/Utils.hs
+++ src/Data/ByteString/Utils.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS -O2 -Wall #-}
 
 module Data.ByteString.Utils
-    (lazifyBS, strictifyBS, randomBS)
+    (lazifyBS, strictifyBS, randomBS, xorBS)
 where
 
 import Control.Monad(replicateM)
@@ -9,6 +9,7 @@ import qualified Data.ByteString as SBS
 import qualified Data.ByteString.Lazy as LBS
 import System.Random(randomIO)
 import Data.Random.Instances()
+import Data.Bits(xor)
 
 strictifyBS :: LBS.ByteString -> SBS.ByteString
 strictifyBS = SBS.concat . LBS.toChunks
@@ -18,3 +19,6 @@ lazifyBS = LBS.fromChunks . return
 
 randomBS :: Int -> IO SBS.ByteString
 randomBS l = SBS.pack `fmap` replicateM l randomIO
+
+xorBS :: SBS.ByteString -> SBS.ByteString -> SBS.ByteString
+xorBS x y = SBS.pack $ SBS.zipWith xor x y
--- src/Db/Guid.hs
+++ src/Db/Guid.hs
@@ -1,18 +1,26 @@
 {-# OPTIONS -O2 -Wall #-}
-{-# OPTIONS -fno-warn-orphans #-}
 
 module Db.Guid
-    (Guid(..), guidLen, newGuid)
+    (Guid(..), guidLen, newGuid, xor)
 where
 
 import qualified Data.ByteString as SBS
-import Data.ByteString.Utils(randomBS)
+import Data.ByteString.Utils(randomBS, xorBS)
 import Data.Binary(Binary(..))
 import Data.Binary.Get(getByteString)
 import Data.Binary.Put(putByteString)
 
 newtype Guid = Guid { guidBS :: SBS.ByteString }
   deriving (Eq, Ord, Read, Show)
+inGuid :: (SBS.ByteString -> SBS.ByteString) -> Guid -> Guid
+inGuid f = Guid . f . guidBS
+inGuid2 :: (SBS.ByteString -> SBS.ByteString -> SBS.ByteString) ->
+           Guid -> Guid -> Guid
+inGuid2 f = inGuid . f . guidBS
+
+instance Binary Guid where
+  get = Guid `fmap` getByteString guidLen
+  put = putByteString . guidBS
 
 guidLen :: Int
 guidLen = 16
@@ -20,6 +28,5 @@ guidLen = 16
 newGuid :: IO Guid
 newGuid = Guid `fmap` randomBS guidLen
 
-instance Binary Guid where
-  get = Guid `fmap` getByteString guidLen
-  put = putByteString . guidBS
+xor :: Guid -> Guid -> Guid
+xor = inGuid2 xorBS
