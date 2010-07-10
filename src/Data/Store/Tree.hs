6456479aca3f004cf15b699ddc7aca0c9a773c78
Eyal Lotem <eyal.lotem@gmail.com>
2010-07-10 14:05:40 +0300

    Refactor Editor.Data.Tree to its own module: Data.Store.Tree

---
+++ src/Data/Store/Tree.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE TypeOperators, TemplateHaskell #-}
+
+module Data.Store.Tree
+    (Tree(..), nodeValueRef, nodeChildrenRefs)
+where
+
+import Control.Monad(liftM2)
+import Data.Binary(Binary(..))
+import Data.IRef(IRef)
+import Data.Record.Label((:->), mkLabels, label)
+
+data Tree a = Node {
+  _nodeValueRef :: IRef a,
+  _nodeChildrenRefs :: [IRef (Tree a)]
+  }
+$(mkLabels [''Tree])
+nodeValueRef :: Tree a :-> IRef a
+nodeChildrenRefs :: Tree a :-> [IRef (Tree a)]
+
+instance Binary (Tree a) where
+  put (Node value children) = put value >> put children
+  get = liftM2 Node get get
--- src/Editor/Data.hs
+++ src/Editor/Data.hs
@@ -1,37 +1,22 @@
 {-# OPTIONS -O2 -Wall #-}
-{-# LANGUAGE TypeOperators, TemplateHaskell #-}
-
 module Editor.Data(
     Tree(..), nodeValueRef, nodeChildrenRefs,
     ITree, ITreeD, TreeD, Data,
     makeValueRef, makeNodeRef, makeLeafRef)
 where
 
-import Control.Monad(liftM2)
 import Data.IRef(IRef)
 import Data.Store(Store)
 import qualified Data.Store as Store
-import Data.Binary(Binary(..))
 import qualified Graphics.UI.VtyWidgets.Grid as Grid
 import qualified Graphics.UI.VtyWidgets.TextEdit as TextEdit
-import Data.Record.Label((:->), mkLabels, label)
+import Data.Store.Tree(Tree(..), nodeValueRef, nodeChildrenRefs)
 
 type Data = ((Grid.Model, Grid.Model), TextEdit.Model)
 type ITreeD = ITree Data
 type TreeD = Tree Data
 
 type ITree a = IRef (Tree a)
-data Tree a = Node {
-  _nodeValueRef :: IRef a,
-  _nodeChildrenRefs :: [ITree a]
-  }
-$(mkLabels [''Tree])
-nodeValueRef :: Tree a :-> IRef a
-nodeChildrenRefs :: Tree a :-> [IRef (Tree a)]
-
-instance Binary (Tree a) where
-  put (Node value children) = put value >> put children
-  get = liftM2 Node get get
 
 makeValueRef :: Store d => d -> String -> IO (IRef Data)
 makeValueRef store text = Store.newIRef store ((Grid.initModel, Grid.initModel), TextEdit.initModel text)
